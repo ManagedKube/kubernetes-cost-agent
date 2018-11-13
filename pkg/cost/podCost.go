83d8bb467ca1f607af59eb2abca0ec6b3b1b0885
garland <garlandk@gmail.com>
2018-11-13 15:14:17 -0800

    adding podcost

---
+++ pkg/cost/podCost.go
@@ -0,0 +1,12 @@
+package cost
+
+type PodCost struct {
+	MinuteMemory float64
+	HourMemory   float64
+	DayMemory    float64
+	MonthMemory  float64
+	MinuteCpu    float64
+	HourCpu      float64
+	DayCpu       float64
+	MonthCpu     float64
+}
